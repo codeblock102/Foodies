2bc5967dea42ec4f0b236839bf90514ad72ab27a
Nazeef <nazeefsuleman@gmail.com>
2024-02-19 10:54:40 -0500

    Initialisation du React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import App from './App.tsx'
 import './index.css'
 
+// Création initiale du router comme fondation de navigation pour les pages de l'application
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App />
+}]); 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
