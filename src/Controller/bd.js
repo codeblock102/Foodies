60cd0e026cee8cf3a1f059e8bcfa7115b9aa8081
Nazeef <nazeefsuleman@gmail.com>
2024-03-09 01:34:58 -0500

    L'utilisateur peut maintenant se logger et creer un compte

--- src/Controller/bd.js
+++ src/Controller/bd.js
@@ -14,12 +14,22 @@ const pool = new Pool({
 // Fonction qui verifie si l'utilisateur est déja inscrit
 async function chercherUtil(nom_util, mot_de_passe) {
   try {
+    // Creer la connexion a la base de donnée
     const client = await pool.connect();
+    // Mettre les valeurs du input dans un array qui sera utilisé dans la requête
     const valeursInputUtil = [nom_util, mot_de_passe];
-    const query =
-      "SELECT * FROM utilisateur WHERE nom_util = $1 AND mot_de_passe = $2 ";
+    const query = "SELECT * FROM utilisateur WHERE nom_util = $1 AND mot_de_passe = $2 ";
 
+    // Encrypter les valeurs pour être capable de les trouver dans la base de donnée
+    // valeursInputUtil.forEach(async (valeur) =>{
+    //   const valeurEncrypter = await bcrypt.hash(valeur,10);
+    //   console.log(await valeurEncrypter);
+    //   return valeurEncrypter;
+    // }); 
+
+    // Faire la requête pour chercher un utilisateur
     const resultat = await client.query(query, valeursInputUtil);
+
     client.release();
     return resultat.rows[0];
   } catch (err) {
@@ -35,14 +45,15 @@ async function creerUtil(infoUtil){
     // Créer la requête SQL pour inscrire l'utilisateur
     const sql = "INSERT INTO utilisateur (prenom,nom_famille,nom_util,courriel,mot_de_passe,date_naissance) VALUES($1,$2,$3,$4,$5,$6)"; 
     // Encrypter les données de l'utilisateur
-    infoUtil = {
-      prenom: await bcrypt.hash(infoUtil.prenom,10),
-      nom: await bcrypt.hash(infoUtil.nom,10),
-      nomUtil: await bcrypt.hash(infoUtil.nomUtil,10),
-      courriel: await bcrypt.hash(infoUtil.courriel,10),
-      motDePasse :await bcrypt.hash(infoUtil.motDePasse,10),
-      date: await bcrypt.hash(infoUtil.date,10),
-    }
+    // infoUtil = {
+    //   prenom: await bcrypt.hash(infoUtil.prenom,10),
+    //   nom: await bcrypt.hash(infoUtil.nom,10),
+    //   nomUtil: await bcrypt.hash(infoUtil.nomUtil,10),
+    //   courriel: await bcrypt.hash(infoUtil.courriel,10),
+    //   motDePasse :await bcrypt.hash(infoUtil.motDePasse,10),
+    //   date: await bcrypt.hash(infoUtil.date,10),
+    // }
+    // Transformer l'objet "infoUtil" en array car la fonctoin client.query ne prends pas d'objet
     const valeursUtilArray = Object.values(infoUtil);
     // Faire la requête 
     const resultat = await client.query(sql,valeursUtilArray);
@@ -54,21 +65,6 @@ async function creerUtil(infoUtil){
     console.log("erreur:", err);
   }
 }
-// Établic la connexion avec la base de donnée
-// pool.connect((err,client, release) =>{
-//   if(err){
-//     return console.error("Erreur de connexion avec la bd:",err);
-//   }
-//   console.log("Connecté à la base de donnée");
-//   client.query(`SELECT * FROM utilisateur`,(err, resultat) =>{
-//     release();
 
-//     if(err){
-//       console.error("Erreur de connexion avec la table",err.stack);
-//     }else{
-//       console.log("resultat:",resultat.rows);
-//     }
-//   })
-// })
 
 module.exports = { chercherUtil,creerUtil };
