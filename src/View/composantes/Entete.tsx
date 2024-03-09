5b21d17d12c39edafae5621f9842be60842e3a25
Nazeef <nazeefsuleman@gmail.com>
2024-03-09 15:25:14 -0500

    Ajout du style pour l'entete

--- src/View/composantes/Entete.tsx
+++ src/View/composantes/Entete.tsx
@@ -1,11 +1,14 @@
-import '../../style.css';
-import '../../index.css';
+import "../../style.css";
+import "../../index.css";
+import parametres_menu from "../../assets/parametres_menu.svg";
 
 export default function Entete() {
   return (
-    <div className="header Entete">
-            <h1>Foodies</h1>
-            <div className="settings"></div>
+    <div className="header Entete w-screen border-1 border-black flex justify-between h-12 p-2">
+      <h1 className="logo">Foodies</h1>
+      <li className="h-10 w-10 list-none	">
+        <img src={parametres_menu} alt="" className="h-8 w-8" />
+      </li>
     </div>
-  )
+  );
 }
