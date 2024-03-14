134c6fb9f8e549bc465fd69b5509a8f1595261bc
Nazeef <nazeefsuleman@gmail.com>
2024-03-14 13:02:13 -0400

    Ameliorations du style de la page d'exploration

--- src/View/Exploration.tsx
+++ src/View/Exploration.tsx
@@ -1,17 +1,20 @@
 import Publimini from "./composantes/Publimini";
 import Entete from "./composantes/Entete";
 import MenuNav from "./composantes/MenuNav";
-import "../style.css";
+import {Input} from "@nextui-org/react";
+import {Form} from "react-router-dom";
+
+//import "../style.css";
 import "../index.css";
 
 export default function Exploration() {
   return (
     <div className="Exploration">
       <Entete />
-      <form action="">
-        <input type="text" placeholder="chercher" className="barre-recherche" />
-      </form>
-      <div className="posts flex w-screen mx-auto ml-0.5">
+      <Form className="h-fit">
+      <input type="text" placeholder="chercher" className="barre-recherche h-4 pl-2" />
+      </Form>
+      <div className="posts flex w-screen mx-auto ml-0.5 m-14">
         <Publimini />
         <Publimini />
         <Publimini />
